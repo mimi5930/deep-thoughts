86ed6775e698dabfa5aa76724ba7a4aeedb302cc
Mike <mikej.miller440@gmail.com>
2022-04-13 21:31:33 -0500

    connected servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+	createHttpLink
 } from '@apollo/client';
 
 import Header from './components/Header';
@@ -13,26 +13,26 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql'
+	uri: '/graphql'
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+	link: httpLink,
+	cache: new InMemoryCache()
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
-        </div>
-        <Footer />
-      </div>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className="flex-column justify-flex-start min-100-vh">
+				<Header />
+				<div className="container">
+					<Home />
+				</div>
+				<Footer />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
-  // use useQuery hook to make query request
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
+	// use useQuery hook to make query request
+	const { loading, data } = useQuery(QUERY_THOUGHTS);
 
-  const thoughts = data?.thoughts || [];
+	const thoughts = data?.thoughts || [];
 
-  return (
-    <main>
-      <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="flex-row justify-space-between">
+				<div className="col-12 mb-3">
+					{loading ? (
+						<div>Loading...</div>
+					) : (
+						<ThoughtList
+							thoughts={thoughts}
+							title="Some Feed for Thought(s)..."
+						></ThoughtList>
+					)}
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Home;
