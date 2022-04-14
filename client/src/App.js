63005cf6d694ab6f32bee02438c75512172af0e8
Mike <mikej.miller440@gmail.com>
2022-04-13 22:40:33 -0500

    added react routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,14 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// react-router imports
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 const httpLink = createHttpLink({
 	uri: '/graphql'
 });
@@ -24,13 +32,33 @@ const client = new ApolloClient({
 function App() {
 	return (
 		<ApolloProvider client={client}>
-			<div className="flex-column justify-flex-start min-100-vh">
-				<Header />
-				<div className="container">
-					<Home />
+			<Router>
+				<div className="flex-column justify-flex-start min-100-vh">
+					<Header></Header>
+					<div className="container">
+						{/* Switch makes it default to one w/out path */}
+						<Switch>
+							<Route exact path="/" component={Home}></Route>
+							<Route exact path="/login" component={Login}></Route>
+							<Route exact path="/signup" component={Signup}></Route>
+							{/* question mark makes it optional */}
+							<Route
+								exact
+								path="/profile/:username?"
+								component={Profile}
+							></Route>
+							<Route
+								exact
+								path="/thought/:id"
+								component={SingleThought}
+							></Route>
+
+							<Route component={NoMatch}></Route>
+						</Switch>
+					</div>
+					<Footer></Footer>
 				</div>
-				<Footer />
-			</div>
+			</Router>
 		</ApolloProvider>
 	);
 }
